27cbf55c75553a547b04264a0e225c0eac2bb6a7
Emre Tuncer <ecantuncer@gmail.com>
2025-11-22 20:27:15 +0300

    feat(GAP-3): Implemented all data models based on API contract

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,39 @@
+class UserModel {
+  final int id;
+  final String username;
+  final String email;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+
+  UserModel({
+    required this.id,
+    required this.username,
+    required this.email,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      username: json['username'],
+      email: json['email'],
+      createdAt: json['createdAt'] != null 
+          ? DateTime.parse(json['createdAt']) 
+          : null,
+      updatedAt: json['updatedAt'] != null 
+          ? DateTime.parse(json['updatedAt']) 
+          : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'username': username,
+      'email': email,
+      'createdAt': createdAt?.toIso8601String(),
+      'updatedAt': updatedAt?.toIso8601String(),
+    };
+  }
+}
